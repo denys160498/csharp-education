aa8a238a71716d0a67718f613c047a006b6149cc
Denys Petrenko <petrenko160498@gmail.com>
2021-01-28 09:51:46 +0200

    Added training project YieldOperator

---
+++ Projects/YieldOperator/YieldOperator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace YieldOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Week week = new Week();
+
+            Console.WriteLine("Europe week:");
+
+            foreach (var d in week)
+            {
+                Console.WriteLine(d);
+            }
+
+            Console.WriteLine("\nAmerican week:");
+
+            foreach (var d in week.GetAmericanWeek())
+            {
+                Console.WriteLine(d);
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ Projects/YieldOperator/YieldOperator/Week.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+
+namespace YieldOperator
+{
+    class Week
+    {
+        string[] days;
+
+        public Week()
+        {
+            string[] weekDays = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            days = weekDays;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < days.Length; i++)
+            {
+                yield return days[i];
+            }
+        }
+
+        public IEnumerable GetAmericanWeek()
+        {
+            yield return days[days.Length - 1];
+
+            for (int i = 0; i < days.Length - 1; i++)
+            {
+                yield return days[i];
+            }
+        }
+    }
+}
